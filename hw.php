43483e674689ab5fb30b48e0fb37bcf7dd44d5e5
Mark-PHP-2022 <119440838+Mark-PHP-2022@users.noreply.github.com>
2023-01-08 18:18:57 +0300

    Add files via upload

---
+++ hw.php
@@ -0,0 +1,58 @@
+<?php
+//1.
+$a=10;
+echo $a;
+$a=20;
+echo $a;
+//2.
+$a = 1+2+3;
+echo $a;
+//3.
+$a=10;
+$b=5;
+$c=$a-$b;
+$d=7;
+$result=$c+$d;
+echo $result;
+//4.
+$a=1.5;
+$b=0.75;
+echo $a+$b;
+//5,
+$a=-5;
+
+echo (abs($a));
+//6.
+$a=10;
+$b=3;
+$c=$a%$b;
+echo $c;
+//7.
+$a="я люблю хайперпоп";
+echo (strlen($a));
+//8.
+$a="лет вам сегодня";
+echo (int)$a;
+//9.
+$a='true';
+echo $a;
+//10.
+$a='false';
+echo $a;
+//11.
+$a='null';
+echo $a; 
+//12.
+$a=12345;
+// не знаю(((
+//13.
+ $days=365;
+ $houres=24;
+ $min=60;
+ $sec1=60;
+ $sec=$days*$houres*$min*$sec1;
+ echo $sec;
+ //14.
+ $tf=97;
+ $cl=(97*9/5)+32;
+ echo $cl;
